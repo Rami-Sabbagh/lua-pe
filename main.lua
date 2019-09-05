d53d05eb27ae813b33b425f988b0a0ea08a76e4b
RamiLego4Game (HP-Win10) <ramilego4game@gmail.com>
2019-09-06 01:50:15 +0300

    Parse the supported parts of love.exe, and print some info

--- main.lua
+++ main.lua
@@ -11,6 +11,25 @@ local luaPE = require("lua-pe")
 --Open the .exe file which we want to change it's icon
 local executable = assert(io.open(sourceDirectory.."/love.exe","rb+"))
 
+print("- Parsing love.exe")
+
+local pe = luaPE.newPE(executable) --Get a PE instance with nothing parsed
+pe:parseDOSHeader(true) --Parse the DOS header
+pe:readDOSStub() --Read the DOS stub :P
+pe:parsePEHeader() --Parse the PE header
+pe:parseCOFFHeader() --Parse the COFF header
+
+print("========================")
+print("= love.exe Parsed Info =")
+print("========================")
+
+print("Machine:",pe.coffHeader:getMachineName())
+
+print("Characteristics:",pe.coffHeader.Characteristics)
+for k,v in pairs(pe.coffHeader:getCharacteristicsFlags()) do
+    print(" - "..k.." :",tostring(v))
+end
+
 executable:close() --Close the file
 
 love.event.quit() --End the execution
