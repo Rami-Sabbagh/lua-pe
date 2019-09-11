7f0d3db67bb1571d0a50193a1a9358402447b94c
RamiLego4Game (HP-Win10) <ramilego4game@gmail.com>
2019-09-11 22:10:16 +0300

    WIP Resources parsing

--- lua-pe/pe/resources.lua
+++ lua-pe/pe/resources.lua
@@ -36,9 +36,58 @@ end
 
 --Parse the DOS header, if parseContent is true, then it's content is parsed, which is usually ignored.
 function resources:parse(parseContent)
-    
+    --Calculate the resources offset
+    local ResourcesRVA = self.peOptHeader.DataDirectory[3].VirtualAddress
+    local ResourcesOffset = self.sectionTable:convertRVA2Offset(ResourcesRVA)
 
+    --Seek to the resources start
+    self.file:seek(ResourcesOffset,"set")
+
+    self.resources, self.meta = self:_parseDirectoryTable()
+    
     return self
+end
+
+--==Internal methods==--
+
+--Parse a directory table and return it
+function resources:_parseDirectoryTable()
+    local meta = {
+        Characteristics = self.file:readLong(),
+        TimeDateStamp = self.file:readLong(),
+        MajorVersion = self.file:readShort(),
+        MinorVersion = self.file:readShort(),
+        NumberOfNameEntries = self.file:readShort(),
+        NumberOfIDEntries = self.file:readShort(),
+
+        NameEntries = {},
+        IDEntries = {},
+        Entries = {}
+    }
+
+    for i=1, meta.NumberOfNameEntries + meta.NumberOfIDEntries do
+        local entry = {}
+
+        if i <= meta.NumberOfNameEntries then
+            entry.NameOffset = self.file:readLong()
+            meta.NameEntries[#meta.NameEntries + 1] = entry
+        else
+            entry.ID = self.file:readLong()
+            meta.IDEntries[#meta.NameEntries + 1] = entry
+        end
+
+        local Offset = self.file:readLong()
+
+        if Offset >= 0x80000000 then
+            entry.SubdirectoryOffset = Offset - 0x80000000
+        else
+            entry.DataEntryOffset = Offset
+        end
+        
+        meta.Entries[#meta.Entries + 1] = entry
+    end
+
+    return {}, meta
 end
 
 return resources --Provide the class
